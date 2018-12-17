166408af262ed4ffd4429274e1875e7aacd36130
jimit200 <jimit.gosar200@gmail.com>
2018-12-17 09:32:15 -0600

    started practice

---
+++ Chapter 7 Notes/PersonWithZip.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+/**
+ * this class holds the zip code and name of the person.
+ *
+ * @jimit200    
+ * @12/17/18
+ */
+public class PersonWithZip
+{
+    // instance variables 
+    private String firstName;
+    private String lastName;
+    private int zipCode;
+    /**
+     * Constructor for objects of class PersonWithZip
+     */
+    public PersonWithZip(String initialFirstName,String initialLastName,int initialZipCode)
+    {
+        // initialise instance variables
+        firstName=initialFirstName;
+        lastName= initialLastName;
+        zipCode=initialZipCode;
+    }
+
+    /**
+     * returns the zip code
+     */
+    public int getZip()
+    {
+
+        return zipCode;
+    }
+    
+    /*
+     * returns the first name and last name in a readable format
+     */
+    public String toString(){
+    
+    String combined = ("Name: " + firstName+" "+lastName+" zip: "+zipCode);
+    return combined;
+    }
+    
+    public static void main(String args[]){
+    
+        PersonWithZip[] arrayZips  = new PersonWithZip[3];
+        Scanner s = new Scanner(System.in);
+        for(int i =0; i< arrayZips.length;i++){
+            
+        System.out.println("Enter first name, last name, and zip code for person #"+(i+1) + "with fields separated by a space");
+        arrayZips[i] = new PersonWithZip(s.next(),s.next(),s.nextInt());
+        
+        }
+        for (PersonWithZip person : arrayZips){
+        System.out.println(person);
+        
+        }
+        
+        System.out.print("The following person has the least numbered zip code ");
+        int leastZip = arrayZips[0].getZip(); 
+        for(int i =1; i< arrayZips.length;i++){
+         if(arrayZips[i].getZip()<leastZip){
+            leastZip = arrayZips[i].getZip()
+            }
+         
+         
+        }
+    }
+}
